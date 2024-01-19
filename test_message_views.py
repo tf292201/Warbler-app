f4dee8280d7a5bc42a555e3ccd8d9cee4162acb7
tf292201 <tf292201@gmail.com>
2024-01-19 13:39:18 -0500

    more testing
    more testing

--- test_message_views.py
+++ test_message_views.py
@@ -1,73 +1,67 @@
-"""Message View tests."""
-
-# run these tests like:
-#
-#    FLASK_ENV=production python -m unittest test_message_views.py
-
-
 import os
+import unittest
 from unittest import TestCase
 
-from models import db, connect_db, Message, User
-
-# BEFORE we import our app, let's set an environmental variable
-# to use a different database for tests (we need to do this
-# before we import our app, since that will have already
-# connected to the database
-
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
-
-
-# Now we can import app
-
+from models import db, Message, User
 from app import app, CURR_USER_KEY
-
-# Create our tables (we do this here, so we only create the tables
-# once for all tests --- in each test, we'll delete the data
-# and create fresh new clean test data
-
-db.create_all()
-
-# Don't have WTForms use CSRF at all, since it's a pain to test
-
-app.config['WTF_CSRF_ENABLED'] = False
-
 
 class MessageViewTestCase(TestCase):
     """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
+        os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
-        User.query.delete()
-        Message.query.delete()
-
+        # Create a test client
         self.client = app.test_client()
 
-        self.testuser = User.signup(username="testuser",
-                                    email="test@test.com",
-                                    password="testuser",
-                                    image_url=None)
+        # Set up the application context
+        with app.app_context():
+            # Create tables
+            db.create_all()
 
-        db.session.commit()
+            # Check if the user already exists
+            self.testuser = User.query.filter_by(email="test@test.com").first()
+
+            if not self.testuser:
+                # Add sample data
+                self.testuser = User.signup(username="testuser",
+                                            email="test@test.com",
+                                            password="testuser",
+                                            image_url=None)
+
+                db.session.commit()
+
+                # Refresh the user instance to avoid DetachedInstanceError
+                self.testuser = User.query.get(self.testuser.id)
+
+    def tearDown(self):
+        """Clean up resources after each test."""
+        with app.app_context():
+            db.session.rollback()
+            db.drop_all()
 
     def test_add_message(self):
         """Can use add a message?"""
 
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
-
         with self.client as c:
             with c.session_transaction() as sess:
                 sess[CURR_USER_KEY] = self.testuser.id
 
             # Now, that session setting is saved, so we can have
-            # the rest of ours test
-
+            # the rest of our tests
             resp = c.post("/messages/new", data={"text": "Hello"})
 
+            print(resp.get_data(as_text=True))
             # Make sure it redirects
-            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.status_code, 200)
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    # Other test methods...
+
+if __name__ == '__main__':
+    unittest.main()
